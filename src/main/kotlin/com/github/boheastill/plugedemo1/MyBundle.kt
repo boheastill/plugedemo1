10ec9d0c7822fa1d17d3f95ca1a34a724a531b75
GitHub Action <action@github.com>
2022-07-20 08:44:27 +0000

    Template cleanup

--- src/main/kotlin/com/github/boheastill/plugedemo1/MyBundle.kt
+++ src/main/kotlin/com/github/boheastill/plugedemo1/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.boheastill.plugedemo1
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/boheastill/plugedemo1/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/boheastill/plugedemo1/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.boheastill.plugedemo1.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.boheastill.plugedemo1.MyBundle
 
 class MyApplicationService {
 
